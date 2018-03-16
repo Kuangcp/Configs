864a3ef4c949c684c1104600f3be4ad92bd23898
mythos <kuang.myth@foxmail.com>
2018-03-16 09:42:33 +0800

    分离配置

--- Linux/init.sh
+++ Linux/init.sh
@@ -6,9 +6,10 @@ help(){
     end='\033[0m'
     echo "运行：dash init.sh $start <params> $end"
     printf "  $start%-16s$end%-20s\n" "-h|h|help" "帮助"
-    printf "  $start%-16s$end%-20s\n" "app  z|ba" "是常用命令以及脚本的短别名 并且追加到bashrc或者zshrc文件中去"
-    printf "  $start%-16s$end%-20s\n" "path z|ba" "是环境变量存放文件"
-    printf "  $start%-16s$end%-20s\n" "git  z|ba" "git仓库别名文件"
+    printf "  $start%-16s$end%-20s\n" "sys   z|ba" "是常用命令简写 并且追加到bashrc或者zshrc文件中去"
+    printf "  $start%-16s$end%-20s\n" "path  z|ba" "是环境变量存放文件"
+    printf "  $start%-16s$end%-20s\n" "repo  z|ba" "git仓库别名文件"
+    printf "  $start%-16s$end%-20s\n" "cus  z|ba" "自定义的脚本工具"
 }
 append(){
     echo 'if [ -f ~/.'$1' ]; then
@@ -21,17 +22,21 @@ case $1 in
     -h | h |help)
         help
     ;;
-    app)
-        ln -s $path'/init/app_short.conf' ~/.app_short
-        append app_short $2
+    sys)
+        ln -s $path'/init/system.conf' ~/.system
+        append system $2
     ;;
-    git)
-        ln -s $path'/init/git_repo.conf' ~/.git_repo
-        append git_repo $2
+    repo)
+        ln -s $path'/init/repos.conf' ~/.repos
+        append repos $2
     ;;
     path)
         ln -s $path'/init/path.conf' ~/.path
         append path $2
+    ;;
+    cus)
+        ln -s $path'/init/customized.conf' ~/.customized
+        append customized $2
     ;;
     *)
         help
