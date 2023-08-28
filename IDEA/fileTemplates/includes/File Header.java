b6d47e0bed9dc4b1a3aa9e28c0cc273a0889cf00
Kuangcp <kuangcp@aliyun.com>
2023-08-28 23:38:21 +0800

    config: idea

--- IDEA/fileTemplates/includes/File Header.java
+++ IDEA/fileTemplates/includes/File Header.java
@@ -1,4 +1,4 @@
 /**
  *
- * @author https://github.com/kuangcp on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ * @author <a href="https://github.com/kuangcp">Kuangcp</a> on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
  */
