ca7e468378a55a762be2d5e007afe0b4501dbbb8
Kuangcp <kuangcp@aliyun.com>
2024-05-06 23:58:26 +0800

    jmc desktop

---
+++ Linux/jmc-run.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/home/kcp/.sdkman/candidates/java/17.0.10-oracle
+export JRE_HOME=${JAVA_HOME}/jre
+export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
+export PATH=${JAVA_HOME}/bin:$PATH
+
+GTK_THEME=vimix-light-doder /home/kcp/Application/Tool/jmc-9.0.0_linux-x64/JDK-Mission-Control/jmc
