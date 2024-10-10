17a4a78cd7bdb4222aa85b43185c9d4e23168971
kuangcp <kuangcp@aliyun.com>
2024-10-10 15:06:45 +0800

    js

---
+++ Tampermonkey/CopyLink.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         CopyLink
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-10
+// @description  复制当前链接为MD
+// @author       Mythos
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mubu.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  let Container = document.createElement("div");
+  Container.id = "md-link-copier";
+  Container.style.position = "fixed";
+  Container.style.left = "80px";
+  Container.style.top = "600px";
+  Container.style["z-index"] = "999999";
+  Container.innerHTML = `<button id="copy-link-btn" style="position:absolute; left:30px; top:20px; background-color:#7597de;border-radius: 1px;border: none;padding: 3px;"> Link </button>`;
+
+  let append = false;
+  Container.addEventListener("click", function () {
+    if (!append) {
+      let txtDiv = document.createElement("input");
+      txtDiv.id = "tmp-text";
+      txtDiv.style["z-index"] = "-999999";
+      //txtDiv.style.display = "none";
+      let link = "> [" + document.title + "](" + document.location.href + ")  ";
+      txtDiv.value = link;
+      // console.log(link);
+      document.body.appendChild(txtDiv);
+      append = true;
+    }
+
+    const input = document.querySelector("#tmp-text");
+    // console.log(input);
+    input.select();
+    document.execCommand("copy");
+
+    Container.innerHTML = `<button id="copy-link-btn" style="position:absolute; left:30px; top:20px; background-color:#e67e22;border-radius: 1px;border: none;padding: 3px;"> Link </button>`;
+  });
+
+  document.body.appendChild(Container);
+})();
