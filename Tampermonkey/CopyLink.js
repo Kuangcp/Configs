45c53abe809cc88a9c232de2fed021c882d889ff
kcp <kuangcp@aliyun.com>
2025-01-09 14:37:02 +0800

    delay style

--- Tampermonkey/CopyLink.js
+++ Tampermonkey/CopyLink.js
@@ -16,34 +16,46 @@
   if (window !== window.top) {
     return;
   }
+  let originBtnH5 = `<button id="copy-link-btn" style="position:absolute; left:30px; top:20px; background-color:#53AD53;border-radius: 8px;border: none;padding: 3px;"> Copy </button>`;
+  let clickBtnH5 = `<button id="copy-link-btn" style="position:absolute; left:30px; top:20px; background-color:#2C2E33;border-radius: 8px;border: none;padding: 3px;"> Copied!! </button>`;
   let Container = document.createElement("div");
   Container.id = "md-link-copier";
   Container.style.position = "fixed";
   Container.style.left = "1vw";
   Container.style.top = "90vh";
   Container.style["z-index"] = "999999";
-  Container.innerHTML = `<button id="copy-link-btn" style="position:absolute; left:30px; top:20px; background-color:#7597de;border-radius: 1px;border: none;padding: 3px;"> Link </button>`;
+  Container.innerHTML = originBtnH5;
 
   let append = false;
   Container.addEventListener("click", function () {
+    let tid = "tmp-text";
     if (!append) {
       let txtDiv = document.createElement("input");
-      txtDiv.id = "tmp-text";
+      txtDiv.id = tid;
       txtDiv.style["z-index"] = "-999999";
-      //txtDiv.style.display = "none";
+      txtDiv.style["display"] = "none";
       let link = "> [" + document.title + "](" + document.location.href + ")  ";
       txtDiv.value = link;
       // console.log(link);
       document.body.appendChild(txtDiv);
       append = true;
+    } else {
+      let txtDiv = document.getElementById(tid);
+      let link = "> [" + document.title + "](" + document.location.href + ")  ";
+      txtDiv.value = link;
     }
 
-    const input = document.querySelector("#tmp-text");
+    const input = document.querySelector("#" + tid);
     // console.log(input);
     input.select();
     document.execCommand("copy");
 
-    Container.innerHTML = `<button id="copy-link-btn" style="position:absolute; left:30px; top:20px; background-color:#e67e22;border-radius: 1px;border: none;padding: 3px;"> Link </button>`;
+    Container.innerHTML = clickBtnH5;
+
+    function restore() {
+      Container.innerHTML = originBtnH5;
+    }
+    setTimeout(restore, 1800);
   });
 
   document.body.appendChild(Container);
